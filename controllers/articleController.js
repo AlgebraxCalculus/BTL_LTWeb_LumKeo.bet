a683b63837d79b87c446b9669da0b8836e32944a
HnimNarT <156636071+Hnim2910@users.noreply.github.com>
2025-04-21 18:55:54 +0700

    Add files via upload

---
+++ controllers/articleController.js
@@ -0,0 +1,20 @@
+const Article = require('../models/Article');
+
+exports.createArticle = async (req, res) => {
+  try {
+    const article = new Article(req.body);
+    await article.save();
+    res.status(201).json(article);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
+
+exports.getAllArticles = async (req, res) => {
+  try {
+    const articles = await Article.find();
+    res.status(200).json(articles);
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
+};
